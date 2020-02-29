8012002548cb5295554409566afb99220025cb5f
Robin Appelman <robin@icewind.nl>
2020-02-29 19:42:38 +0100

    update to new timer hal

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use esp8266_hal::ehal::digital::v2::OutputPin;
 use esp8266_hal::ehal::serial::Write;
 use esp8266_hal::ehal::timer::CountDown;
 use esp8266_hal::gpio::GpioExt;
-use esp8266_hal::timer::{Nanoseconds, Timer, TimerExt};
+use esp8266_hal::timer::{Nanoseconds, TimerExt};
 use esp8266_hal::uart::UART0Ext;
 
 const TEXT: &'static str = "Hello world!\r\n";
@@ -24,25 +24,21 @@ fn main() -> ! {
     let dp = unsafe { esp8266::Peripherals::steal() };
     let pins = dp.GPIO.split();
     let mut pin = pins.gpio2.into_push_pull_output();
-    let mut timer = dp.TIMER.timer(CORE_HZ);
+    let (_, mut timer2) = dp.TIMER.timers(CORE_HZ);
     let mut serial = dp
         .UART0
         .serial(pins.gpio1, pins.gpio3, pins.gpio13, pins.gpio15);
 
+    timer2.start(Nanoseconds(500_000_000));
     loop {
         pin.set_high().unwrap();
-        sleep_ms(100_000_000, &mut timer);
+        nb::block!(timer2.wait()).unwrap();
         for byte in TEXT.bytes() {
             serial.write(byte).unwrap();
         }
         pin.set_low().unwrap();
-        sleep_ms(900_000_000, &mut timer);
+        nb::block!(timer2.wait()).unwrap();
     }
-}
-
-fn sleep_ms(duration: u32, timer: &mut Timer) {
-    timer.start(Nanoseconds(duration));
-    nb::block!(timer.wait()).unwrap();
 }
 
 /// Basic panic handler - just loops
