f67c957335518cea51add49ecb67ae14dd10bb59
Robin Appelman <robin@icewind.nl>
2020-08-23 19:31:25 +0200

    update to esp8266-hal 0.2

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,25 @@
 use xtensa_lx106_rt::entry;
 
 use core::panic::PanicInfo;
-use esp8266_hal::ehal::digital::v2::OutputPin;
-use esp8266_hal::ehal::digital::v2::ToggleableOutputPin;
-use esp8266_hal::ehal::timer::{CountDown};
-use esp8266_hal::gpio::GpioExt;
-use esp8266_hal::timer::{TimerExt, Nanoseconds};
-
-/// The default clock source is the onboard crystal
-/// In most cases 40mhz (but can be as low as 2mhz depending on the board)
-/// Clock speed is then doubled from the crystal frequency
-const CORE_HZ: u32 = 80_000_000;
+use esp8266_hal::prelude::*;
+use esp8266_hal::target::Peripherals;
 
 #[entry]
 fn main() -> ! {
-    let dp = unsafe { esp8266::Peripherals::steal() };
+    // The default clock source is the onboard crystal
+    // In most cases 40mhz (but can be as low as 2mhz depending on the board)
+    // Clock speed is then doubled from the crystal frequency
+    let clock_frequency = 80.mhz();
+
+    let dp = unsafe { Peripherals::steal() };
     let pins = dp.GPIO.split();
     let mut led = pins.gpio2.into_push_pull_output();
-    let (mut timer1, _) = dp.TIMER.timers(CORE_HZ);
-    timer1.start(Nanoseconds(100_000_000));
+    let (mut timer1, _) = dp.TIMER.timers(clock_frequency);
 
     led.set_high().unwrap();
 
     loop {
-        nb::block!(timer1.wait()).unwrap();
+        timer1.delay_ms(1000);
         led.toggle().unwrap();
     }
 }
